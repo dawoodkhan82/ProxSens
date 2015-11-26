d8117b5f5b1dbc222b47f385edea2dfaf97b395f
Dawood Khan <DKhan@dawoods-air.nyc.rr.com>
2015-11-25 23:53:12 -0500

    Initial Commit

---
+++ ProxSens/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProxSens
+//
+//  Created by Dawood Khan on 11/25/15.
+//  Copyright © 2015 Dawood Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
