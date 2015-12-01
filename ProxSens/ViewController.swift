54447325247ae98aa765069342da867484488b4a
dawoodkhan82 <dawoodkhan82@gmail.com>
2015-11-30 22:46:56 -0500

    ProxSens App
    Full application code included.

--- ProxSens/ViewController.swift
+++ ProxSens/ViewController.swift
@@ -7,12 +7,59 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    func proximityChanged(notification: NSNotification) {
+        let device = notification.object as? UIDevice
+        if device?.proximityState == true {
+            print("\(device) detected!")
+            toggleTorch(on: true)
+        } else {
+            toggleTorch(on: false)
+        }
+    }
+    
+    
+    func activateProximitySensor() {
+        let device = UIDevice.currentDevice()
+        device.proximityMonitoringEnabled = true
+        if device.proximityMonitoringEnabled {
+            NSNotificationCenter.defaultCenter().addObserver(self, selector: "proximityChanged:", name: "UIDeviceProximityStateDidChangeNotification", object: device)
+            
+        }
+    }
+    
+    func toggleTorch(on on: Bool) {
+        let device = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        
+        if device.hasTorch {
+            do {
+                try device.lockForConfiguration()
+                
+                if on == true {
+                    device.torchMode = .On
+                } else {
+                    device.torchMode = .Off
+                }
+                
+                device.unlockForConfiguration()
+            } catch { 
+                print("Torch could not be used")
+            }
+        } else {
+            print("Torch is not available")
+        }
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        activateProximitySensor()
+        
     }
 
     override func didReceiveMemoryWarning() {
